83f2fbb8a85da564abdd06a143796302ab795180
100613 <100613@oryx.edu.qa>
2022-09-14 19:16:31 +0300

    initial commit

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 //first NodeJS application
-constexpress=require('express')
-constapp=express()
-constport=3000
+const express=require('express')
+const app = express()
+const port=3000
 app.get('/',(req,res)=>{
   res.send('Hello Virtual Machine!')
 })
